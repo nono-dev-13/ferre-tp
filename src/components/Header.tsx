3ca35d82604af5e5e55c0c63a20350e7cf63c9bf
nono-dev-13 <arnaud.boubli19@gmail.com>
2026-02-03 11:06:58 +0100

    refacto navigation desktop and mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import { JSX } from 'react';
 import { ThemeToggle } from '@/components/ThemeToggle';
 import Link from 'next/link';
-import DesktopNav from '@/components/DesktopNav';
-import { MobileNav } from '@/components/MobileNav';
+import { MainNav } from '@/components/MainNav';
 
 function Header(): JSX.Element {
   return (
     <header className="fixed top-0 left-0 w-full bg-primary text-primary-foreground p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">FERRE TP</h1>
-        <DesktopNav />
-        <MobileNav />
-        <ThemeToggle />
+        <Link href="/" aria-label="Accueil">
+          <h1 className="text-xl font-bold">FERRE TP</h1>
+        </Link>
+        <div className="flex items-center gap-4">
+          <MainNav />
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
