38e23df53c996f3514663451725c5dbbdd013314
nono-dev-13 <arnaud.boubli19@gmail.com>
2026-02-11 14:10:23 +0100

    Add favicon, openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,25 @@ export const metadata: Metadata = {
   title: 'FERRE TP Entreprise de travaux publics',
   description:
     'FERRE TP, entreprise de travaux publics, Services et environnement, location TP, terrassement, transport gravats matériaux boues, drainage dans la région de Chartres',
+  openGraph: {
+    title: 'FERRE TP Entreprise de travaux publics',
+    description:
+      'FERRE TP, entreprise de travaux publics, Services et environnement, location TP, terrassement, transport gravats matériaux boues, drainage dans la région de Chartres',
+    url: 'https://ferre-tp.com',
+    siteName: 'FERRE TP',
+    images: [
+      {
+        url: 'https://ferre-tp.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'fr_FR',
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -16,7 +35,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className="antialiased min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">{children}</main>
