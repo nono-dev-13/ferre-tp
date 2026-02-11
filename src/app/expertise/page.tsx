fe7f10440d06190b6f388d04dc445f225dab0443
nono-dev-13 <arnaud.boubli19@gmail.com>
2026-02-11 11:30:50 +0100

    filter expertise

--- src/app/expertise/page.tsx
+++ src/app/expertise/page.tsx
@@ -1,18 +1,50 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 import { metiers } from '@/data/metiers';
 import { Text } from '@/components/Text';
 import { ArrowRight } from 'lucide-react';
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 function Expertise() {
+  const [selectedMetier, setSelectedMetier] = useState<string>('all');
+  const onChangeExpertise = (metierId: string) => {
+    setSelectedMetier(metierId);
+  };
+  const metiersToDisplay =
+    selectedMetier === 'all' ? metiers : metiers.filter((metier) => metier.id === selectedMetier);
   return (
     <section className="container mx-auto p-4 mb-8 pt-35">
-      <Text variant="h1" className=" mb-12">
-        Les métiers de Ferre TP
-      </Text>
+      <div className="flex flex-col md:flex-row gap-8 items-center md:items-start mb-8">
+        <Text variant="h1">Les métiers de Ferre TP</Text>
+        <Select defaultValue="all" onValueChange={onChangeExpertise}>
+          <SelectTrigger className="w-full max-w-54">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="all">Tous les métiers</SelectItem>
+              {metiers.map((metier) => (
+                <SelectItem key={metier.id} value={metier.id}>
+                  {metier.title}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
 
       <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {metiers.map((metier) => (
+        {metiersToDisplay.map((metier) => (
           <li key={metier.id} className="flex">
             <Card>
               <CardHeader>
